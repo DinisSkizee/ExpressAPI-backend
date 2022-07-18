7544f7d229a07d36ce410ee1e68fbe2775bea528
Flávio Dinis de Oliveira Figueiredo Lopes da Silva <dinissilvauk@gmail.com>
2022-07-18 19:32:19 +0100

    minor fix

--- routes/friends.router.js
+++ routes/friends.router.js
@@ -1,9 +1,12 @@
 const express = require("express");
 
-const friendsController = require("./controllers/friends.controller");
+const friendsController = require("../controllers/friends.controller");
 
 const friendsRouter = express.Router();
 
+friendsRouter.use((req, res, next) => {
+  console.log("ip address:", req.ip);
+});
 friendsRouter.get("/", friendsController.getFriends);
 friendsRouter.get("/:friendId", friendsController.getFriendById);
 friendsRouter.post("/", friendsController.postFriends);
